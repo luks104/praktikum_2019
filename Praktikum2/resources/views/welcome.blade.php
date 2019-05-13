ddeb49f0c787aef96236f0df7a378d290565fa48
jurkko <jure.turk@student.um.si>
2019-05-13 17:10:48 +0200

    test

--- Praktikum2/resources/views/welcome.blade.php
+++ Praktikum2/resources/views/welcome.blade.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Laravel</title>
+        <h1>Zajc trenira github</h1>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
