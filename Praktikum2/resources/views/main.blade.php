e385583f0405f1543f1d42dace2f2ecec7bb27d7
jurkko <jure.turk@student.um.si>
2019-05-18 23:06:30 +0200

    editor

---
+++ Praktikum2/resources/views/main.blade.php
@@ -0,0 +1,55 @@
+
+@extends('layouts.app')
+
+@section('content')
+
+
+<!doctype html>
+<html lang="en">
+  <head>
+   
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    
+    
+    <script src='https://cloud.tinymce.com/5/tinymce.min.js?apiKey=u10v0g64egvz9bbbguv1pg14x0au3nus6yzv88vhiwbfwd8v'></script>
+
+    <script>
+    tinymce.init({
+        selector: '#mytextarea'
+    });
+    </script>
+
+    <title>Hello, world!</title>
+    </head>
+
+    <body>
+    <div style="height: 100px">
+    
+    </div>
+    <div class="row" style="height: 500px">
+        <div class="col-lg-2">
+
+    
+        </div>
+        <div class="col-lg-8">
+        <form method="post">
+            <textarea id="mytextarea">Hello, World!</textarea>
+            <button type="button" class="btn btn-primary">Primary</button>
+        </form>
+    
+        </div>
+        <div class="col-lg-2">
+
+
+        </div>
+    </div>
+    
+    
+    </body>
+    @endsection
+</html>
+
+
+
