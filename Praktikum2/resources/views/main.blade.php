40d939f708ad1d9b67bbb27516a2f436ecf3373d
jurkko <jure.turk@student.um.si>
2019-05-18 23:12:49 +0200

    lul

--- Praktikum2/resources/views/main.blade.php
+++ Praktikum2/resources/views/main.blade.php
@@ -10,9 +10,6 @@
    
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    
-    
     <script src='https://cloud.tinymce.com/5/tinymce.min.js?apiKey=u10v0g64egvz9bbbguv1pg14x0au3nus6yzv88vhiwbfwd8v'></script>
 
     <script>
@@ -21,8 +18,8 @@
     });
     </script>
 
-    <title>Hello, world!</title>
-    </head>
+    <title>main</title>
+</head>
 
     <body>
     <div style="height: 100px">
--- Praktikum2/routes/web.php
+++ Praktikum2/routes/web.php
@@ -18,3 +18,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+//routi gredo v homecontroller?
+Route::get('/main', function () { 
+    return view('main');
+});
