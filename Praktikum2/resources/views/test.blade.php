06938c0da6df56081e13fb98aae6c3240d1ee075
KnightFury1 <papiyamatata@gmail.com>
2019-05-21 13:29:38 +0100

    Barebone osnova za vstavljanje komponent

--- Praktikum2/resources/views/test.blade.php
+++ Praktikum2/resources/views/test.blade.php
@@ -10,52 +10,39 @@
 
 <script>
  var editor_config = {
-      path_absolute : "{{ URL::to('/') }}/",
-      selector : "textarea",
-      plugins: [
+  selector: 'textarea#editor',
+  toolbar: 'komponente | undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image media',
+  plugins: [
         "advlist autolink lists link image charmap print preview hr anchor pagebreak",
         "searchreplace wordcount visualblocks visualchars code fullscreen",
-        "insertdatetime media nonbreaking save table contextmenu directionality",
         "emoticons template paste textcolor colorpicker textpattern",
        
       ],
-      toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image media",
-      relative_urls: false,
-      file_browser_callback : function(field_name, url, type, win) {
-        var x = window.innerWidth || document.documentElement.clientWidth || document.getElementByTagName('body')[0].clientWidth;
-        var y = window.innerHeight|| document.documentElement.clientHeight|| document.grtElementByTagName('body')[0].clientHeight;
-        var cmsURL = editor_config.path_absolute+'laravel-filemanaget?field_name'+field_name;
-        if (type = 'image') {
-          cmsURL = cmsURL+'&type=Images';
-        } else {
-          cmsUrl = cmsURL+'&type=Files';
-        }
-
-        tinyMCE.activeEditor.windowManager.open({
-          file : cmsURL,
-          title : 'Filemanager',
-          width : x * 0.8,
-          height : y * 0.8,
-          resizeble : 'yes',
-          close_previous : 'no'
-        });
+      
+  setup: (editor) => {
+      editor.ui.registry.addButton('komponente', {
+        text: 'Komponenta',
+      tooltip: 'Insert Current Date',
+      onAction: function (_) {
+        editor.insertContent("<input readonly style='margin-left:5px;margin-right:5px;border-radius:2px;' placeholder='Enterlabel' data-label='nekiLabel'>");
       }
-    };
+      });
+    }
+    
+  };
 
     tinymce.init(editor_config);
 
     
-  </script>﻿
+  </script> 
   </head>
     <body>
-    <div class="row" style="height: 500px">
+    <div class="row">
             <div class="col-lg-2">
-
-        
             </div>
             <div class="col-lg-8">
-            <form method="post">
-                <textarea id="textarea">Kaj zaj kurva boš delal al neboš</textarea>
+            <form method="post" class="form-group">
+                <textarea id="editor" style="height:30em;"></textarea>
                 <button type="button" class="btn btn-primary">Primary</button>
             </form>
         
@@ -64,7 +51,8 @@
 
 
             </div>
-        </div>
+    </div>
+    
     
     </body>
     @endsection
