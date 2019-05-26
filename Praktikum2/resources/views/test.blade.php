a921843cc7ac4f0e4c13248c0422d872ff31350c
KnightFury1 <papiyamatata@gmail.com>
2019-05-26 10:08:35 +0200

    Editor naprej z boljo potjo

--- Praktikum2/resources/views/test.blade.php
+++ Praktikum2/resources/views/test.blade.php
@@ -10,37 +10,11 @@
 
 <script>
 
-class Component{
-  constructor(position,isSet){
-    this.position=position;
-    this.isSet = isSet;
-  }
-  getPosition(){
-    return this.position;
-  }
-  getIsSet(){
-    return this.isSet;
-  }
-}
-
-function findComponent(){
-  let currentComponent=0 ;
-    for(let i =0;i<components.length;i++){
-      if(components[i].isSet === false){
-        currentComponent=i;break;
-      }
-      else {
-        currentComponent = components.length;
-      }
-    }
-    return currentComponent;
-}
-let components = [];
 
 
  var editor_config = {
   selector: 'textarea#editor',
-  toolbar: 'components | undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image media| saveComponent',
+  toolbar: 'components | undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image media| button1',
   plugins: [
         "advlist autolink lists link image charmap print preview hr anchor pagebreak",
         "searchreplace wordcount visualblocks visualchars code fullscreen",
@@ -49,7 +23,19 @@ let components = [];
       ],
       
   setup: (editor) => {
-      
+      editor.ui.registry.addButton('button1', {
+        text: 'Yeet',
+        onAction: function (_) {
+          tinymce.activeEditor.dom.select('input');
+          let compets = tinymce.activeEditor.dom.select('input.component');
+          let label = [];
+          for(let y =0;y<compets.length;y++){
+             label.push(compets[y].attributes.name.value);
+          }
+
+      }
+      });
+
       editor.ui.registry.addMenuButton('components', {
         text: 'Components',
         fetch: function (callback) {
@@ -58,36 +44,20 @@ let components = [];
               type: 'menuitem',
               text: 'Komponenta',
               onAction: function(_){
-                let tempComp = findComponent();
-                components[tempComp] = new Component(tempComp,true);
-                console.log(components);
-                editor.insertContent("<input data-group='+ group + ' data-label='FOO_label' data-id=" + tempComp +" id='komponenta' type='text' class='form-control komponenta' style='width:15%;height:calc(0.59rem + 2px);padding:.375rem .75rem;font-size:.9rem;line-height:1.6;color:#495057;background-color:#fff;background-clip:padding-box;border:1px solid #ced4da;border-radius:.25rem;margin-left: .5rem;margin-right:.5rem;transition:border-color .15s ease-in-out,box-shadow .15s ease-in-out' placeholder='Enterlabel'>");
+                let compets = tinymce.activeEditor.dom.select('input.component');
+                editor.insertContent("<input name='' data-label='FOO_label' id="+compets.length+" type='text' class='form-control component' style='width:15%;height:calc(0.59rem + 2px);padding:.375rem .75rem;font-size:.9rem;line-height:1.6;color:#495057;background-color:#fff;background-clip:padding-box;border:1px solid #ced4da;border-radius:.25rem;margin-left: .5rem;margin-right:.5rem;transition:border-color .15s ease-in-out,box-shadow .15s ease-in-out' placeholder='Enter your label...'>");
+                let compets2 = tinymce.activeEditor.dom.select('input.component');
+                console.log(compets2);
+                for(let y =0;y<compets2.length;y++){
+                  tinymce.activeEditor.dom.setAttrib(compets2[y], 'name', 'WEAREHERE');
+                } 
               }
             }
           ];
           callback(menuItems);
         }
       });
-      editor.on('DblClick', function(evt) {
-      if(evt.target.id == 'komponenta'){
-        let id = evt.target.getAttribute("data-id");
-        console.log("Removed component: "+id);
-        components[id] = new Component(id,false);
-        evt.target.remove();
-
-        //evt.target.setAttribute("data-label", evt.target.value);
-      }
       
-    });
-      editor.on('Click', function(evt) {
-      if(evt.target.id == 'komponenta'){
-        let id = evt.target.getAttribute("data-id");
-        console.log("Updated component: "+id +" with label: "+evt.target.value);
-
-        evt.target.setAttribute("data-label", evt.target.value);
-      }
-      
-    });
 
     }
   };
@@ -112,8 +82,6 @@ let components = [];
             <li class="list-group-item">DVOJNI KLIK na textbox izbrise komponento. NE BRISI Z BACKSPACE-OM</li>
             <li class="list-group-item">Komponente majo svoj data-id da jih lakha js locim</li>
             <li class="list-group-item">F12-> Console izpisuje vn ka se dogaja ko klikas oz. dvojno-klikas</li>
-      
-      
           </ul>
     
     </div>
