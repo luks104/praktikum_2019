5c9153a5091c7cd9b3bf922976683b639d8de861
Marko <marko.zmazek@student.um.si>
2019-05-27 12:30:21 +0200

    z DaTA labEL nEGre

--- Praktikum2/resources/views/create.blade.php
+++ Praktikum2/resources/views/create.blade.php
@@ -50,7 +50,7 @@ function enterInput(e){
 //Saves the name of current selected component.
 function saveCurrentComp(){
   let v = tinymce.activeEditor.dom.get(currentComponent).value;
-  tinymce.activeEditor.dom.setAttribs(currentComponent, {'name': v, placeholder: v});
+  tinymce.activeEditor.dom.setAttribs(currentComponent, {'data-label': v, placeholder: v});
   leaveInput();
   checkContent();
 }
@@ -82,7 +82,7 @@ function checkContent(){
   }
   else{
     for(let y =0;y<comps.length;y++){
-      if(tinymce.activeEditor.dom.getAttrib(comps[y], 'name')=== ''){
+      if(tinymce.activeEditor.dom.getAttrib(comps[y], 'data-label')=== ''){
         found=true; break;
       }
     }
@@ -115,7 +115,7 @@ function checkContent(){
               text: 'Component',
               onAction: function(_){
                 let compets = tinymce.activeEditor.dom.select('input.component');
-                editor.insertContent("<input  name='' id="+compets.length+" type='text' class='form-control component' style='width:15%;height:calc(0.59rem + 2px);padding:.375rem .75rem;font-size:.9rem;line-height:1.6;color:#495057;background-color:#fff;background-clip:padding-box;border:1px solid #ced4da;border-radius:.25rem;margin-left: .5rem;margin-right:.5rem;transition:border-color .15s ease-in-out,box-shadow .15s ease-in-out' placeholder='Enter your label...'>");
+                editor.insertContent("<input  name='' id="+compets.length+" type='text' class='form-control component' style='width:15%;height:calc(0.59rem + 2px);padding:.375rem .75rem;font-size:.9rem;line-height:1.6;color:#495057;background-color:#fff;background-clip:padding-box;border:1px solid #ced4da;border-radius:.25rem;margin-left: .5rem;margin-right:.5rem;transition:border-color .15s ease-in-out,box-shadow .15s ease-in-out' placeholder='Enter your label...' data-label=''>");
                 let compets2 = tinymce.activeEditor.dom.select('input.component');
                 //console.log(compets2);
               }
