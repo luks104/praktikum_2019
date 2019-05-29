1d4559ec81b7351c39b49a3b2bc9d98381d39050
luks104 <luka.lukec10@hotmail.com>
2019-05-29 09:49:39 +0200

    Update form.blade.php

--- Praktikum2/resources/views/forms/form.blade.php
+++ Praktikum2/resources/views/forms/form.blade.php
@@ -11,11 +11,7 @@
         {!! $form->form_data !!}
     </div>
 
-<<<<<<< HEAD
     <a href="{{ route('formWizard') }}" class="btn btn-primary">Izpolni obrazec {{$form->form_name}}</a>
-=======
-    <a href="/form/{{$form->id}}/wizard" class="btn btn-primary">Izpolni obrazec {{$form->form_name}}</a>
->>>>>>> 5cea98e58a718feebc011f15408aac6f705b9984
     <a href="/form/{{$form->id}}/formToPDF" class="btn btn-primary">pretvori v PDF {{$form->form_name}}</a>
     
 </div>
