05cea295af347d1b34bc435786196745812b61f0
KnightFury1 <papiyamatata@gmail.com>
2019-06-03 18:02:33 +0200

    Output design completed

--- Praktikum2/resources/views/output.blade.php
+++ Praktikum2/resources/views/output.blade.php
@@ -2,53 +2,62 @@
 
 @section('content')
 
+<div class="container">
+    <div style="height:5em;">
 
-
-<div class='row'>
-    <div class="col s1"></div>
-    <div class="col s5">
-    <h3>Form: {{$form->form_name}}</h3>
-    <font color="black"><h4 font color="black">Form data:</h4></font>
-    {!! $data !!}
+    </div>
+    <div class="row">
+        <div class="card-panel col l8 offset-m1 offset-l2 s12 m10 z-depth-5" style="padding:0;background-color:#08aeea80">    
+            <div class="white-text center-align" style="padding:2em;background-color:#08aeea;font-size:1.2em;">You have successfully completed the form.</div>
+            <div class="white" style="height:0.1em;" ></div>
+            <div style="margin-top:5em;margin-bottom:5em;">
+                <div class="row" >
+                    <div class="col l8 offset-l2">
+                            <a href="/form/{{$form->id}}/wizard/formToPDF">
+                            <div class="card-panel hoverable valign-wrapper animated bounceInRight" style="padding:0.1em;margin:0.3em;margin-top:0.6em;" >
+                                    <div class="col" style="border-right:1px solid #576071;">
+                                        <i class="material-icons red-text">picture_as_pdf</i>
+                                    </div>                                            
+                                    <div class="col  s12 m12 l12 left-align">
+                                        <h6 class="ff-poppins" style="letter-spacing:1.5px;padding:0;color:#576071;min-height:1.2em;"> Export as a PDF file</h6>
+                                    </div>
+                            </div>
+                            </a>
+                    </div>
+                </div>
+                <div class="row ">
+                        <div class="col l8 offset-l2">
+                                <a href="/form/{{$form->id}}/wizard/formToPDF">
+                                <div class="card-panel hoverable valign-wrapper animated bounceInLeft" style="padding:0.1em;margin:0.3em;margin-top:0.6em;" >
+                                        <div class="col" style="border-right:1px solid #576071;">
+                                            <i class="material-icons blue-text">description</i>
+                                        </div>                                            
+                                        <div class="col  s12 m12 l12 left-align">
+                                            <h6 class="ff-poppins" style="letter-spacing:1.5px;padding:0;color:#576071;min-height:1.2em;"> Export as a DOC file</h6>
+                                        </div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="row ">
+                            <div class="col l8 offset-l2">
+                                    <a href="/form/{{$form->id}}/wizard/formToPDF">
+                                    <div class="card-panel hoverable valign-wrapper animated bounceInRight" style="padding:0.1em;margin:0.3em;margin-top:0.6em;" >
+                                            <div class="col" style="border-right:1px solid #576071;">
+                                                <i class="material-icons green-text">mail</i>
+                                            </div>                                            
+                                            <div class="col  s12 m12 l12 left-align">
+                                                <h6 class="ff-poppins" style="letter-spacing:1.5px;padding:0;color:#576071;min-height:1.2em;"> Send the form as mail</h6>
+                                            </div>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+            </div>
+        </div>
        
     </div>
-
-    <div class="col s5" id="1" style=" padding-top: 30px;">
-        <div class="row animated fadeInUp left-align" style="margin:0;">
-            <div class="col s12 m10 offset-m1 l10 ">
-            <a href="/form/{{$form->id}}/wizard/formToPDF">
-                <div class="card-panel hoverable valign-wrapper" style="padding:0.1em;margin:0.3em;margin-top:0.6em;" >
-                    <div class="col" style="border-right:1px solid #576071;">
-                        <i class="material-icons" class="bgStill" style="color:red">picture_as_pdf</i>
-                    </div>                                            
-                    <div class="col  s12 m12 l12 left-align">
-                        <h6 class="ff-poppins" style="letter-spacing:1.5px;padding:0;color:#576071;min-height:1.2em;"> Export as a PDF file<span class="red-text">{{  $form->categorie_name  }}</span></h6>
-                    </div>
-                </div>
-            </a>
-            </div>
-        </div>
-    </div>
-    <div class="col s5" id="1" style=" padding-top: 30px;">
-        <div class="row animated fadeInUp left-align" style="margin:0;">
-            <div class="col s12 m10 offset-m1 l10 ">
-            <a href="">
-                <div class="card-panel hoverable valign-wrapper" style="padding:0.1em;margin:0.3em;margin-top:0.6em;" >
-                    <div class="col" style="border-right:1px solid #576071;">
-                        <i class="material-icons" class="bgStill">description</i>
-                    </div>                                            
-                    <div class="col  s12 m12 l12 left-align">
-                        <h6 class="ff-poppins" style="letter-spacing:1.5px;padding:0;color:#576071;min-height:1.2em;"> Export as a DOC file<span class="red-text">{{  $form->categorie_name  }}</span></h6>
-                    </div>
-                </div>
-            </a>
-            </div>
-        </div>
-    </div>
-
-    <div class="col s1"></div>
-
-
+        
 </div>
 
 @endsection
